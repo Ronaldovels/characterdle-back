8c3549199f0c3775cf800a45efde1f8903aa229c
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2024-10-03 18:19:14 -0300

    Update character.js

--- routes/character.js
+++ routes/character.js
@@ -9,13 +9,14 @@ const characterSchema = new mongoose.Schema({
     race: String,
     hair_color: String,
     eye_color: String,
+    introducion_arc: String,
 })
 
 const Character = mongoose.model('Character', characterSchema)
 
 router.get('/', async (req, res) => {
     try {
-        const {name, gender, filiation, race, hair_color, eye_color} = req.query
+        const {name, gender, filiation, race, hair_color, eye_color, introducion_arc} = req.query
 
         const filter = {}
 
@@ -31,6 +32,7 @@ router.get('/', async (req, res) => {
         if(race) filter.race = race
         if(hair_color) filter.hair_color = hair_color
         if(eye_color) filter.eye_color = eye_color
+        if(introducion_arc) filter.introducion_arc = introducion_arc
 
 
         const character = await Character.find(filter)
@@ -48,7 +50,8 @@ router.post('/', async (req, res) => {
             filiation: req.body.filiation,
             race: req.body.race,
             hair_color: req.body.hair_color,
-            eye_color: req.body.eye_color
+            eye_color: req.body.eye_color,
+            introducion_arc: req.body.introducion_arc
         })
         
         await newCharacter.save()
@@ -57,6 +60,35 @@ router.post('/', async (req, res) => {
     } catch (error) {
         res.status(400).json({error: 'Failed to create record'})
     }
+})
+
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const updateCharacter = await Character.findByIdAndUpdate(id,
+            {
+                name: req.body.name,
+                gender: req.body.gender,
+                filiation: req.body.filiation,
+                race: req.body.race,
+                hair_color: req.body.hair_color,
+                eye_color: req.body.eye_color,
+                introducion_arc: req.body.introducion_arc
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        if (!updateCharacter) {
+            return res.status(404).json({error: 'Character not found'})
+        }
+        res.json(updateCharacter)
+    } catch (error) {
+        res.status(400).json({error: "Can't update character"})
+    }
+
 })
 
 module.exports = router
