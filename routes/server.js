65c809f94f4ad1c956bdc8e44d38f45193b27209
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2024-10-11 22:26:14 -0300

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -36,4 +36,4 @@ const keepAlive = () => {
   };
   
   // Envia um ping a cada 13 minutos para manter a API ativa
-  setInterval(keepAlive, 13 * 60 * 1000);
+  setInterval(keepAlive, 7 * 60 * 1000);
